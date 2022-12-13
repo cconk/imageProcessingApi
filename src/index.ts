8f6fe60823115eadca1179b3c476102e729ccd02
chadc <chadconklin@outlook.com>
2022-12-13 13:34:07 -0800

    fixing things up

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,25 @@ const app = express()
 
 const port = 3000
 
-app.get('/api', (req, res) => {
+const logger = (
+  req: express.Request,
+  res: express.Response,
+  next: Function
+): void => {
+  const url = req.url;
+  console.log(`${url} was visited`);
+  next();
+};
+
+app.get('/', logger, (req, res) => {
+  
   res.send(
     '<h1>Welcome Home</h1>'
   );
-})
+});
 
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
-})
+});
 
 
