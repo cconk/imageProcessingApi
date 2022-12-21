fe60a57739387f3fed17ee65c8223adae2c099bc
chadc <chadconklin@outlook.com>
2022-12-20 16:40:44 -0800

    updates with working file editing and caching

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -3,14 +3,35 @@ import ImageUtilities from '../../utilities/utilities';
 
 const images = express.Router();
 
-images.get('/', async (req: express.Request, res: express.Response): Promise<void> => {
+images.get('/', (req: express.Request, res: express.Response) => {
   res.send('base images url');
   console.log(req.params);
 });
 
-images.get('/:filename', async (req: express.Request, res: express.Response): Promise<void> => {
-  console.log(ImageUtilities.buildFilePath(req.params.filename));
-  res.sendFile(ImageUtilities.buildFilePath(req.params.filename));
+images.get('/testing', async (req: express.Request, res: express.Response) => {
+  const filename = req.query.filename?.toString();
+  const height = req.query.height?.toString();
+  const width = req.query.width?.toString();
+  //check for query params
+  if (!filename || !height || !width) {
+    res.send('Please enter all necessary parameters for your file to be diplayed including filename, height and width.')
+  } else {
+    //check to see if file exists
+    const thumbFileCheck = await ImageUtilities.checkForThumbFile(filename).then((value) => {
+      return value;
+    });
+    if (!thumbFileCheck) {
+      //if no file exists then create it based on imaged given and return it to browser compensating for processing delay
+      await ImageUtilities.buildThumbFile(filename, Number(height), Number(width)).then(() => {
+        res.setTimeout(4000, () => {
+          res.sendFile(ImageUtilities.buildThumbFilePath(filename));
+        });
+      });
+    } else {
+        //if file is cached return it with no processing delay
+        res.sendFile(ImageUtilities.buildThumbFilePath(filename));
+    }
+  }
 });
 
 export default images;
--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,13 +1,40 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import sharp from 'sharp';
 
-const imagesPath = path.resolve(__dirname, '../../images');
+const imagesPath = path.resolve(__dirname, '../../images/normal');
+const thumbImagesPath = path.resolve(__dirname, '../../images/thumbs')
 const fileExtension = '.jpg'
 
 export default class ImageUtilities {
 
   //seting up as a function so additional file types png gif etc. might be used in the future
-  static buildFilePath(filename: string): string {
+  static buildFilePath(filename?: string): string {
     return `${ imagesPath }\\${filename}${fileExtension}`;
+  }
+
+  //builds the thumb file path to send to the route
+  static buildThumbFilePath(filename?: string, height?: number, width?: number): string {
+    return `${thumbImagesPath}\\${filename}${fileExtension}`;
+  }
+
+  //creates the new thumb file if it does not already exist
+  static async buildThumbFile(filename?: string, height?: number, width?: number) {
+    const thumbPath = this.buildThumbFilePath(filename);
+    const path = this.buildFilePath(filename);
+    await sharp(path).resize(height, width).toFile(thumbPath, (err: any, info: any) => {
+      return;
+    });
+  }
+
+  //checks to see if the file already exists
+  static async checkForThumbFile(filename?: string): Promise<boolean> {
+    const thumbPath = this.buildThumbFilePath(filename);
+    try {
+      await fs.access(thumbPath) 
+        return true;
+    } catch {
+        return false;
+    }
   }
 }
